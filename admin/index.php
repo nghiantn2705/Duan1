b2a9990e0a4858674b5ddb74f5c16bfec9576222
nghiantn2705 <117350106+nghiantn2705@users.noreply.github.com>
2022-11-14 21:49:41 +0700

    Du An 1

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php 
+include "../model/pdo.php";
+include "./header.php";
+// Controller
+if(isset($_GET['act'])){
+    $act = $_GET['act'];
+    switch($act){
+        case 'adddm':
+            include "danhmuc/add.php";
+            break;
+        case 'listdm':
+            include "danhmuc/list.php";
+            break;
+        
+            
+        default:
+            include "view/home.php";
+            break;
+    }
+}
+else{
+    include "view/home.php";
+}
+include "view/footer.php";
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+include "model/pdo.php";
+include "global.php";
+include "view/header.php";
+if((isset($_GET['act'])) && ($_GET['act']!="")){
+    $act =$_GET['act'];
+    switch ($act){
+        case 'dangky':
+            break;
+        case 'dangnhap':
+            break;
+        case 'gopy':
+            include "view/gopy.php";
+            break;
+        case 'hoidap':
+            include "view/hoidap.php";
+            break;
+            
+        default:
+            include "view/home.php";
+            break;
+    }
+}
+else{
+    include "view/home.php";
+}
+include "view/footer.php";
+?>
