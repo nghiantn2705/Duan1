f6ef6ddd1a61759b73498a1099dfd59592897d75
Your Name <sonpvph27505@fpt.edu.vn>
2022-12-13 20:02:38 +0700

    hieubentau

--- admin/bill/updatebill.php
+++ admin/bill/updatebill.php
@@ -11,9 +11,15 @@
                 <form action="index.php?act=updatebills" method="post" enctype="multipart/form-data">
                
                         <label for="">Tình trạng đơn hàng</label>
-                        <input type="text" name="bill_status" value="<?php echo $bill_status;?>">
+                        <!-- <input type="number" name="bill_status" min="0" max="2" value="<?php echo $bill_status;?>"> -->
+                        <select name="bill_status" id="">
+                        <option value="<?php echo $bill_status;?>">Cập nhật trạng thái đơn hàng</option>
+                            <option value="0">Đơn hàng mới</option>
+                            <option value="1">Đang xử lý</option>
+                            <option value="2">Đang giao hàng</option>
+                        </select>
                         <div class=" d-flex justify-content-center">
-                            <input type="hidden" class="btn btn-success " name="bill_id" value="<?=$bill_id?>">
+                            <input type="hidden" class="btn btn-success "  name="bill_id" value="<?=$bill_id?>">
                             <input type="submit" class="btn btn-success " name="capnhat" value="CẬP NHẬT">
                             <input type="reset" class="btn btn-success" value="NHẬP LẠI">
                             <a href="index.php?act=listbill"><input type="button" class="btn btn-success" value="DANH SÁCH"></a>    
